19b326091b8c36f8c93cd308ec634ea66102c8df
Carlos Martinez <curlosmart@gmail.com>
2022-03-21 10:42:47 -0400

    Add new CORS urls

--- src/main/java/com/example/invoiceapp/controller/InvoiceController.java
+++ src/main/java/com/example/invoiceapp/controller/InvoiceController.java
@@ -23,14 +23,14 @@ public class InvoiceController {
     }
 
     // Get all invoices
-    @CrossOrigin(origins = "http://localhost:4200/")
+    @CrossOrigin(origins = {"http://localhost:4200/", "https://invoice-app-beta.vercel.app/"})
     @GetMapping
     public List<Invoice> getAllInvoices() {
         return invoiceService.getAllInvoices();
     }
 
     // Save a new invoice
-    @CrossOrigin(origins = {"http://localhost:4200", "https://invoice-app-springboot.herokuapp.com"})
+    @CrossOrigin(origins = {"http://localhost:4200/", "https://invoice-app-beta.vercel.app/"})
     @PostMapping
     public ResponseEntity<Invoice> saveInvoice(@RequestBody Invoice invoice) {
         System.out.println(invoice);
@@ -38,21 +38,21 @@ public class InvoiceController {
     }
 
     // Get invoice by id
-    @CrossOrigin(origins = "http://localhost:4200/")
+    @CrossOrigin(origins = {"http://localhost:4200/", "https://invoice-app-beta.vercel.app/"})
     @GetMapping("{id}")
     public Invoice getInvoiceById(@PathVariable("id") long id) {
         return invoiceService.getInvoiceById(id);
     }
 
     // Update invoice
-    @CrossOrigin(origins = "http://localhost:4200/")
+    @CrossOrigin(origins = {"http://localhost:4200/", "https://invoice-app-beta.vercel.app/"})
     @PutMapping("{id}")
     public ResponseEntity<Invoice> updateEmployee(@PathVariable("id") long id, @RequestBody Invoice invoice){
         return new ResponseEntity<Invoice>(invoiceService.updateInvoice(invoice, id), HttpStatus.OK);
     }
 
     // Delete invoice
-    @CrossOrigin(origins = "http://localhost:4200/")
+    @CrossOrigin(origins = {"http://localhost:4200/", "https://invoice-app-beta.vercel.app/"})
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id){
 
